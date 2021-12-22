976ff43973bdd69658e2ece60df53827d78aa424
Nikita <infiless@ya.ru>
2021-12-22 16:50:08 +0300

    start

---
+++ ft_printf.c
@@ -0,0 +1,5 @@
+//
+// Created by Nikita on 22.12.2021.
+//
+
+#include "ft_printf.h"
---
+++ ft_printf.h
@@ -0,0 +1,8 @@
+//
+// Created by Nikita on 22.12.2021.
+//
+
+#ifndef FT_PRINTF_FT_PRINTF_H
+#define FT_PRINTF_FT_PRINTF_H
+
+#endif //FT_PRINTF_FT_PRINTF_H
